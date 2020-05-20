f15507f41f89da0e9b77ae304aef4b07a55f2c28
Elias Calado <eliascalado@hotmail.com>
2020-05-20 11:30:48 -0300

    criacao do projeto

---
+++ src/main/java/br/com/suporteti/sedevendas/SedevendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.suporteti.sedevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedevendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/suporteti/sedevendas/SedevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suporteti.sedevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
