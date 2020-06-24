ee66d6586e754230d2ac11b8a3c0fad7f651aa7d
Elias Calado <eliascalado@hotmail.com>
2020-06-24 14:13:23 -0300

    Email HTML

--- src/main/java/br/com/suporteti/sedevendas/services/EmailService.java
+++ src/main/java/br/com/suporteti/sedevendas/services/EmailService.java
@@ -1,12 +1,20 @@
 package br.com.suporteti.sedevendas.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.suporteti.sedevendas.domain.Pedido;
 
 public interface EmailService {
+	
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
