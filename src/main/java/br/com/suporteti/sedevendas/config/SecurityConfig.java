f76262c8ef09e2b2343b94b7af56d102ea9acffc
Elias Calado <eliascalado@hotmail.com>
2020-06-26 11:47:50 -0300

    Implementando autorizacao

--- src/main/java/br/com/suporteti/sedevendas/config/SecurityConfig.java
+++ src/main/java/br/com/suporteti/sedevendas/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.suporteti.sedevendas.security.JWTAuthenticationFilter;
+import br.com.suporteti.sedevendas.security.JWTAuthorizationFilter;
 import br.com.suporteti.sedevendas.security.JWTUtil;
 
 @Configuration
@@ -49,7 +50,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
